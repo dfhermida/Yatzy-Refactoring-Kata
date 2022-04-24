6227332d2be140ba1cfcc155795e17da882dcfd7
David Fernández Hermida <dfernandez@gradiant.org>
2022-04-24 12:10:55 +0200

    refactor: test_fours descriptive name

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -32,7 +32,7 @@ def test_threes_scores_sum_dice_reading_3():
     assert 12 == Yatzy.threes(2, 3, 3, 3, 3)
 
 
-def test_fours_test():
+def test_fours_scores_sum_dice_reading_4():
     assert 12 == Yatzy(4, 4, 4, 5, 5).fours()
     assert 8 == Yatzy(4, 4, 5, 5, 5).fours()
     assert 4 == Yatzy(4, 5, 5, 5, 5).fours()
