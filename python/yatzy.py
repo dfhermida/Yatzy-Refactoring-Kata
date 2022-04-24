75fbc9557df5532e5449c7ee637956f44d64f8fa
David Fernández Hermida <dfernandez@gradiant.org>
2022-04-24 12:24:45 +0200

    refactor: ones using array count

--- python/yatzy.py
+++ python/yatzy.py
@@ -16,19 +16,8 @@ class Yatzy:
 
     @staticmethod
     def ones(d1, d2, d3, d4, d5):
-        sum = 0
-        if d1 == 1:
-            sum += 1
-        if d2 == 1:
-            sum += 1
-        if d3 == 1:
-            sum += 1
-        if d4 == 1:
-            sum += 1
-        if d5 == 1:
-            sum += 1
-
-        return sum
+        dice = [d1, d2, d3, d4, d5]
+        return dice.count(1)
 
     @staticmethod
     def twos(d1, d2, d3, d4, d5):
