b49f30c41195e1cee7444fd3b0b305321fe3c20b
David Fernández Hermida <dfernandez@gradiant.org>
2022-04-24 12:17:54 +0200

    refactor: test_four_of_a_kind descriptive name

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -68,7 +68,7 @@ def test_three_of_a_kind_scores_sum_three_matching_dice_0_otherwise():
     assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
 
 
-def test_four_of_a_knd():
+def test_four_of_a_kind_scores_sum_four_matching_dice_0_otherwise():
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
     assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
