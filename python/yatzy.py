16a6e8ea6b5a9c429101b9bc03e4fc138a160823
David Fernández Hermida <dfernandez@gradiant.org>
2022-04-24 11:52:26 +0200

    refactor: chance use sum of dice array

--- python/yatzy.py
+++ python/yatzy.py
@@ -1,13 +1,8 @@
 class Yatzy:
     @staticmethod
     def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+        dice = [d1, d2, d3, d4, d5]
+        return sum(dice)
 
     @staticmethod
     def yatzy(dice):
