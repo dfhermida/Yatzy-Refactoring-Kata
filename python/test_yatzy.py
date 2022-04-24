17ac1087291f65b7fb32c2b0788bdc547cf6ec75
David Fernández Hermida <dfernandez@gradiant.org>
2022-04-24 11:59:02 +0200

    refactor: test_yatzy descriptive name

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -9,7 +9,7 @@ def test_chance_scores_sum_of_all_dice():
     assert 16 == Yatzy.chance(3, 3, 4, 5, 1)
 
 
-def test_yatzy_scores_50():
+def test_yatzy_scores_50_if_all_dice_same_number_0_otherwise():
     assert 50 == Yatzy.yatzy([4, 4, 4, 4, 4])
     assert 50 == Yatzy.yatzy([6, 6, 6, 6, 6])
     assert 0 == Yatzy.yatzy([6, 6, 6, 6, 3])
