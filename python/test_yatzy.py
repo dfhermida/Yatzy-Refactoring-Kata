70111c83e82f5829a70cb58b401795b4e44cbfbe
David Fernández Hermida <dfernandez@gradiant.org>
2022-04-24 11:56:16 +0200

    refactor: test_chance uniform expected actual assertion

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -5,9 +5,7 @@ from yatzy import Yatzy
 
 
 def test_chance_scores_sum_of_all_dice():
-    expected = 15
-    actual = Yatzy.chance(2, 3, 4, 5, 1)
-    assert expected == actual
+    assert 15 == Yatzy.chance(2, 3, 4, 5, 1)
     assert 16 == Yatzy.chance(3, 3, 4, 5, 1)
 
 
