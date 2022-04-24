4150beb13231ff91ae203a87ad59f48b2f4ee54b
David Fernández Hermida <dfernandez@gradiant.org>
2022-04-24 12:29:23 +0200

    refactor: class contructor array declaration

--- python/yatzy.py
+++ python/yatzy.py
@@ -30,12 +30,7 @@ class Yatzy:
         return dice.count(3) * 3
 
     def __init__(self, d1, d2, d3, d4, d5):
-        self.dice = [0] * 5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = d5
+        self.dice = [d1, d2, d3, d4, d5]
 
     def fours(self):
         sum = 0
